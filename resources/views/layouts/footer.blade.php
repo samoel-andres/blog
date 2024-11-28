5ab806280f1bfc998e9b3752592d82a91c73902f
Samoel Andres <126998785+samoel-andres@users.noreply.github.com>
2024-11-28 17:43:00 -0600

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer margen-interno">
     <nav class="pie">
-        <a href="https://edutin.com" target="_blank">Desarrollado por &copy; Edutin Academy</a>
+        <a href="https://edutin.com" target="_blank">Desarrollado por &copy; Samoel Andres</a>
     </nav>
 </footer>
